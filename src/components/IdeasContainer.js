b826bcaabc626a75badb77bad12ec7416b5307b1
James Turello <jturello@yahoo.com>
2017-11-01 00:59:13 -0700

    add newIdeaButton and its styles

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -19,9 +19,21 @@ class IdeasContainer extends Component {
     .catch(error => console.log(error))
   }
 
+  addNewIdea = () => {
+    axios.post('/api/v1/ideas', {idea: {title: '', body: ''}})
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div>
+	<div >
+	  <button className="newIdeaButton" onClick={this.addNewIdea} >New Idea</button>
+	</div>
+
 	{this.state.ideas.map((idea) => {
 	  return (
 	    <Idea idea={idea} key={idea.id} />
