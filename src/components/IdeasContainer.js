2cf791104079743f41cad47c0942cae84c8a972a
James Turello <jturello@yahoo.com>
2017-11-09 03:37:20 -0800

    display update notification message

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -29,7 +29,8 @@ class IdeasContainer extends Component {
       const ideas = update(this.state.ideas, { $splice: [[0, 0, response.data]]})
       this.setState({
 	ideas: ideas,
-        editingIdeaId: response.data.id
+	editingIdeaId: response.data.id,
+	notification: ''
       })
     })
     .catch(error => console.log(error))
@@ -40,7 +41,13 @@ class IdeasContainer extends Component {
     const ideas = update(this.state.ideas, { 
       [ideaIndex]: { $set: idea }
     })
-    this.setState({ideas: ideas})
+    this.setState({
+      ideas: ideas,
+      notification: 'Changes saved!'})
+  }
+
+  resetNotification = () => {
+    this.setState({notification: ''})
   }
 
   render() {
@@ -48,12 +55,17 @@ class IdeasContainer extends Component {
       <div>
 	<div >
 	  <button className="newIdeaButton" onClick={this.addNewIdea} >New Idea</button>
+	  <span className="notification">
+	    {this.state.notification}
+	  </span>
 	</div>
 
 	{this.state.ideas.map((idea) => {
 	  if(this.state.editingIdeaId === idea.id) {
 	    return(
-	      <IdeaForm idea={idea} key={idea.id} updateIdea={this.updateIdea} />
+	      <IdeaForm idea={idea} key={idea.id} 
+		updateIdea={this.updateIdea}
+		resetNotification={this.resetNotification} />
 	    )
 	  } else {
 	    return (
